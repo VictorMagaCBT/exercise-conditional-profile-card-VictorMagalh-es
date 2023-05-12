db18d81f6f6e5769f35aa60fa6ed4ff066e9ebd4
VictorMagaCBT <126924738+VictorMagaCBT@users.noreply.github.com>
2023-05-12 18:15:32 +0000

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -28,22 +28,23 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
+  let name = `<h1>${variables.name} ${variables.lastname}</h1>`;
+  let twitter =
+    // reset the website body with the new html output
+    (document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+            ${name}
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/4geeksacademy"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/4geeksacademy"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
-    `;
+    `);
 }
 
 /**
@@ -64,8 +65,8 @@ window.onload = function() {
     github: "alesanchezr",
     linkedin: null,
     instagram: null,
-    name: null,
-    lastname: null,
+    name: "Victor",
+    lastname: "Magalhães",
     role: null,
     country: null,
     city: null
